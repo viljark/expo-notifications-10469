6be5f165e43d255ddd8e9c1a8870a167cf7354a7
Viljar Kärgenberg <viljar.kargenberg@remato.ee>
2020-09-30 15:04:25 +0300

    simple example of notification issue in standalone builds

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Button, Text, View} from 'react-native'
+import * as Notifications from 'expo-notifications'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	const [pushToken, setPushToken] = React.useState(null)
+
+	React.useEffect(() => {
+		async function getToken() {
+			const pushTokenResponse = await Notifications.getExpoPushTokenAsync()
+			const pushToken = pushTokenResponse.data
+			console.log('Received push token', pushToken)
+			setPushToken(pushToken)
+		}
+		getToken()
+
+	}, [])
+
+
+	React.useEffect(() => {
+		Notifications.setNotificationHandler({
+			//  The function should respond with a behavior object within 3 seconds, otherwise the notification will be discarded.
+			// https://docs.expo.io/versions/latest/sdk/notifications/#setnotificationhandlerhandler-notificationhandler--null-void
+			// @ts-ignore
+			handleNotification: async (notification) => {
+				return {
+					shouldPlaySound: true,
+					shouldSetBadge: true,
+					shouldShowAlert: true,
+					priority: Notifications.AndroidNotificationPriority.MAX,
+				}
+			},
+			handleSuccess: (_notificationId) => {
+			},
+			handleError: async (notificationId) => {
+				await Notifications.dismissNotificationAsync(notificationId)
+			},
+		})
+	}, [])
+
+	async function sendRemoteNotification() {
+		await fetch("https://exp.host/--/api/v2/push/send", {
+			"headers": {
+				"accept": "application/json",
+				"content-type": "application/json",
+			},
+			"body": `[{\"to\":\"${pushToken}\",\"title\":\"Remote notification\",\"body\":\"Remote notification body\"}]`,
+			"method": "POST",
+			"credentials": "omit"
+		})
+	}
+
+	function showLocalNotification() {
+		Notifications.scheduleNotificationAsync({
+			content: {
+				title: 'Local notification',
+				body: 'Local notificationbody',
+			},
+			trigger: null
+		})
+	}
+
+	return (
+		<View style={styles.container}>
+			<Button onPress={showLocalNotification} title={"Show local notification"} />
+			<Text>{pushToken}</Text>
+			<Button onPress={sendRemoteNotification} title={"Send remote notification"} />
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
